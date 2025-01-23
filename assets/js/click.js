51c6866e2ddfe0af99465d60ae719801946f85c0
JeeCoin <danumisgoingto@gmail.com>
2025-01-23 17:58:30 +0800

    first commit

---
+++ assets/js/click.js
@@ -0,0 +1,5 @@
+ buyBtn.addEventListener('click', function() {
+        // 使用window.location实现页面跳转
+        // 请将'https://example.com'替换为您想要跳转到的页面的URL
+        window.location.href = 'https://www.baidu.com';
+});
