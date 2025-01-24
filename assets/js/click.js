4457ea5590c4228d85b4ae8d2e623c6115a8505b
JeeCoin <danumisgoingto@gmail.com>
2025-01-24 18:25:19 +0800

    add x home adress

--- assets/js/click.js
+++ assets/js/click.js
@@ -3,3 +3,22 @@
         // 请将'https://example.com'替换为您想要跳转到的页面的URL
         window.location.href = 'https://www.baidu.com';
 });
+
+twiterBtn.addEventListener('click', function() {
+        // 使用window.location实现页面跳转
+        // 请将'https://example.com'替换为您想要跳转到的页面的URL
+        window.location.href = 'https://x.com/JeeCoin64599';
+});
+
+
+copyBtn.addEventListener('click', function() {
+    const textarea = document.createElement('textarea');
+      textarea.value = "";
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+      copyImg.style.display = "none"
+      tickImg.style.display = "inline-block"
+});
+
